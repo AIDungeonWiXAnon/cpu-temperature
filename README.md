1e74a9995fb4c3fed0a5268178f04b88f5572d07
davimk <dpaulmatheson@yahoo.com>
2020-03-23 19:08:12 +0000

    Add existing files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# cpu-temperature-skill
-Mycroft AI skill that speaks the CPU temperature.
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/thermometer-half.svg" card_color="#FD9E66" width="50" height="50" style="vertical-align:bottom"/> CPU Temperature
+Speaks the cpu temperature.
+
+## About
+Speaks the cpu temperature. Value is obtained from thermal_zone0 at /sys/devices/virtual/thermal/thermal_zone0/temp.
+
+## Examples
+* "What is the cpu temperature"
+* "Cpu temperature"
+* "Thermals"
+* "Thermal"
+* "How is the processor doing"
+* "How is the cpu doing"
+* "What is the processor temperature"
+* "Processor temperature"
+
+## Credits
+David
+
+## Category
+**Information**
+
+## Tags
+#Cpu temperature
+#Processor temperature
+#Thermal_zone0
+
---
+++ __init__.py
@@ -0,0 +1,49 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class CpuTemperature(MycroftSkill):
+    """ Speak the CPU temperature.
+
+    The temperature value is obtained from thermal_zone0 temp file at
+    /sys/class/thermal/thermal_zone0/temp
+
+    The user can set the temperature unit to Celsius or Fahrenheit using the skill
+    configuration page.
+    """
+    
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        
+
+    @intent_file_handler('temperature.cpu.intent')
+    def handle_temperature_cpu(self, message):
+        """Open and read temp file.
+
+        File contains a string that is the CPU temperature in Celsius
+        read to the thousandths place with no decimal.
+        """
+        try:
+            with open ('/sys/class/thermal/thermal_zone0/temp') as f:
+                cputempraw = f.read()
+        except:
+            self.log.error("File not found error.")
+            self.speak("Error. I could not find the temperature file.")
+        else:
+            # Retrieve the temperature unit that was set by user from settings.json file.
+            unit_setting = self.settings.get('degree_unit')
+
+            if unit_setting == 'c':
+                # Format and speak temperature in celsius.
+                cputempC = int(cputempraw) // 1000
+                cputempC = str(cputempC)
+                self.speak_dialog('temperature.cpu', {'cputemp': cputempC, 'unit': 'celsius'})
+            if unit_setting == 'f':
+                # Format, convert to fahrenheit, and speak temperature.
+                cputempF = int(cputempraw) // 1000
+                cputempF = int((cputempF * 1.8) + 32)
+                cputempF = str(cputempF)
+                self.speak_dialog('temperature.cpu', {'cputemp': cputempF, 'unit': 'fahrenheit'}) 
+
+def create_skill():
+    return CpuTemperature()
+
